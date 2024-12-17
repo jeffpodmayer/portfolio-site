bed5a73d9101c6fa1f097cc03e34c5dc734bc678
jeffpodmayer <jeff.podmayer@gmail.com>
2024-12-17 11:57:35 -0800

    added timeline component

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -5,36 +5,38 @@ import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+// import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import TimelineOppositeContent, {
+  timelineOppositeContentClasses,
+} from "@mui/lab/TimelineOppositeContent";
 
-export default function OutlinedTimeline() {
+export default function LeftAlignedTimeline() {
   return (
-    <Timeline position="alternate">
+    <Timeline
+      sx={{
+        [`& .${timelineOppositeContentClasses.root}`]: {
+          flex: 0.4,
+        },
+      }}
+    >
       <TimelineItem>
+        <TimelineOppositeContent color="white">
+          Product Manager
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot variant="outlined" />
+          <TimelineDot></TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>Sept 2024 to Present</TimelineContent>
       </TimelineItem>
       <TimelineItem>
+        <TimelineOppositeContent color="white">
+          Full Stack Coding Bootcamp
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
+          <TimelineDot />
         </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="secondary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
+        <TimelineContent>Nov 2023 to October 2024</TimelineContent>
       </TimelineItem>
     </Timeline>
   );
