ed222092fc93e7133fab065864fb4a4810a667a4
jeffpodmayer <jeff.podmayer@gmail.com>
2024-12-27 13:15:46 -0800

    navbar is now responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,25 +7,8 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/"></a>
       <div className={styles.menu}>
-        {/* <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
-          alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        /> */}
-        <ul
-          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
-        >
-          <li>
-            <a href="#about">About</a>
-          </li>
+        <ul className={styles.menuItems}>
           <li>
             <a href="#projects">Projects</a>
           </li>
