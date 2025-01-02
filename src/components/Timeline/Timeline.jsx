04fc19c412c3129a838e24be2b23e6466468e6c8
jeffpodmayer <jeff.podmayer@gmail.com>
2025-01-02 15:24:08 -0800

    fixed scrolling and timeline responsiveness

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Timeline from "@mui/lab/Timeline";
 import history from "../../data/history.json";
-
+import styles from "../Timeline/Timeline.module.css";
 import TimelineEntry from "./TimelineEntry";
 
 const LeftAlignedTimeline = () => {
   return (
-    <Timeline>
-      {history.map((entry, index) => (
-        <TimelineEntry
-          key={index}
-          role={entry.role}
-          organisation={entry.organisation}
-          startDate={entry.startDate}
-          endDate={entry.endDate}
-          experiences={entry.experiences}
-        />
-      ))}
-    </Timeline>
+    <div className={styles.timelineContainer}>
+      <Timeline>
+        {history.map((entry, index) => (
+          <TimelineEntry
+            key={index}
+            role={entry.role}
+            organisation={entry.organisation}
+            startDate={entry.startDate}
+            endDate={entry.endDate}
+            experiences={entry.experiences}
+          />
+        ))}
+      </Timeline>
+    </div>
   );
 };
 
